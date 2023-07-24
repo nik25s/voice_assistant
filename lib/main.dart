ce765b06fd43ebd05e6f13b450769dfd985d12c0
nik25s <nishantsuthar2002@gmail.com>
2023-07-24 12:22:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:translator/pallete.dart';
+import 'voice_assistant.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Allen',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+          scaffoldBackgroundColor: Pallete.whiteColor,
+          appBarTheme: const AppBarTheme(backgroundColor: Pallete.whiteColor)),
+      home: AI(),
+    );
+  }
+}
---
+++ lib/voice_assistant.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:translator/feature_box.dart';
+import 'package:translator/pallete.dart';
+
+class AI extends StatelessWidget {
+  const AI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Allen'),
+        centerTitle: true,
+        leading: Icon(Icons.menu),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle),
+                  ),
+                ),
+                Container(
+                  height: 123,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/virtualAssistant.png'))),
+                )
+              ],
+            ),
+
+            //chat bubble
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin: EdgeInsets.symmetric(horizontal: 30).copyWith(top: 25),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Pallete.borderColor),
+                  borderRadius:
+                      BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Good Morning, what task can I do for you?',
+                  style: TextStyle(
+                      color: Pallete.mainFontColor,
+                      fontSize: 21,
+                      fontFamily: 'Cera Pro'),
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(top: 10, left: 22),
+              child: Text(
+                'Here are a few features',
+                style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    fontWeight: FontWeight.bold,
+                    color: Pallete.mainFontColor,
+                    fontSize: 20),
+              ),
+            ),
+
+            // Features list
+            Column(
+              children: [
+                FeatureBox(
+                  BoxColor: Pallete.firstSuggestionBoxColor,
+                  headerText: 'ChatGPT',
+                  descriptionText:
+                      'A smarter way to stay organized and informed with chatGPT',
+                ),
+                FeatureBox(
+                  BoxColor: Pallete.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText:
+                      'Get inspired and stay creative with your personal assistant powered by Dall-E',
+                ),
+                FeatureBox(
+                  BoxColor: Pallete.thirdSuggestionBoxColor,
+                  headerText: 'Smart Voice Assistant',
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: Icon(Icons.mic),
+      ),
+    );
+  }
+}
